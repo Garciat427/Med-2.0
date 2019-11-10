6547296ae160d1ad1956c1b4f41e0a30f7c3a32f
neil <arifrahman@hotmail.com>
2019-11-10 17:21:22 -0500

    homepage-buttons added, routes work, placeholder pie chart put in

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -8,9 +8,20 @@ class Home extends Component {
             <body>
             <div className="container">
                 <div className="row">
-                    <div className="col s12">Pie chart for diseases + intro</div>
-                    <div className="col s6">Diagnosis Button</div>
-                    <div className="col s6">Trends Button</div>
+                {/* this photo won't load even when i place it in the same folder */}
+                    <div className="col s12">
+                        Pie chart for diseases + intro
+                       
+                        <img src={require ("../components/Images/piechart.jpg")} alt="pie chart" className= "responsive-img" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s6 offset-s2">
+                    <a className="waves-effect waves-light btn-large" href={"/diagnosis"}>Diagnosis</a>
+                    </div>
+                    <div className="col s6 offset-s2">
+                    <a className="waves-effect waves-light btn-large" href={"/trends"}>Trends</a>
+                    </div>
                 </div>
             </div>
             </body>
