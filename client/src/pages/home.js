a6d0a5b15f3d0d11d97c6bdbd6fd638d45d63cc4
neil <arifrahman@hotmail.com>
2019-11-11 10:48:33 -0500

    3 buttons, homepage, diagnosis and trends work, footnote added, placeholder pie chart in place along with an intro

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 
+import DiagnosisBtn from "../components/DiagnosisBtn"
+import TrendsBtn from "../components/TrendsBtn"
+
 import { Link } from "react-router-dom";
 
 class Home extends Component {
@@ -8,20 +11,21 @@ class Home extends Component {
             <body>
             <div className="container">
                 <div className="row">
-                {/* this photo won't load even when i place it in the same folder */}
-                    <div className="col s12">
-                        Pie chart for diseases + intro
-                       
-                        <img src={require ("../components/Images/piechart.jpg")} alt="pie chart" className= "responsive-img" />
+                    <div className="col s8">
+                        <img src={require ("../components/Images/piechart.jpg")} alt="pie chart" className= "responsive-img" />                      
                     </div>
+                    <div className="col s4">
+                        <h3>Welcome to Med 2.0</h3>
+                        <p>
+                            We track and follow trends in illnesses in Canada. Check out our Diagnosis Button if you feel you may be ill. Please note we will be using your location, symptoms and diagnosis for our goal of tracking illness. You are welcome to have a look at our trends page as well to see what kind of deseases are currently a concern in your area. Wishing you Good Health!
+                        </p>
+                    </div>   
                 </div>
+
+
                 <div className="row">
-                    <div className="col s6 offset-s2">
-                    <a className="waves-effect waves-light btn-large" href={"/diagnosis"}>Diagnosis</a>
-                    </div>
-                    <div className="col s6 offset-s2">
-                    <a className="waves-effect waves-light btn-large" href={"/trends"}>Trends</a>
-                    </div>
+                    <DiagnosisBtn/>
+                    <TrendsBtn/>
                 </div>
             </div>
             </body>
