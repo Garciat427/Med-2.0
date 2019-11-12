880b9bd10daa8b439a4043cd04d9770e4624ffb3
Troy Garcia <tg1212@outlook.com>
2019-11-12 14:12:11 -0500

    Added Body specific api controllers and symptom controllers

--- controllers/apiMedicController.js
+++ controllers/apiMedicController.js
@@ -3,9 +3,14 @@
 const axios = require('axios');
 
 const apiMedicUrl = {
-  bodyLoc:"https://priaid-symptom-checker-v1.p.rapidapi.com/body/locations/", //For Req 1 + 2 (Find Body Location Gen + Specific)
-  bodySymp: "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms/", //Req 3 (Find Symptoms Based On Body Location)
-  propSymp: "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms/proposed", // Req 4 + 5 + 6 (Find Symptoms Based Previous Symptoms)
+  //For Req 1 + 2 (Find Body Location Gen + Specific)
+  bodyLoc:"https://priaid-symptom-checker-v1.p.rapidapi.com/body/locations",
+
+  //Req 3 (Find Symptoms Based On Body Location)
+  bodySymp: "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms",
+
+  // Req 4 + 5 + 6 (Find Symptoms Based Previous Symptoms)
+  propSymp: "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms/proposed"
 }
 
 const headers = {
@@ -16,7 +21,7 @@ const headers = {
 
 
 module.exports = {
-  //Req 1 - Body Locations (General)
+  // Req 1 - Get Body Locations (General)
   bodyLocGeneral : (req, res) => {
     //Request Config
     reqUrl = apiMedicUrl.bodyLoc;
@@ -30,10 +35,46 @@ module.exports = {
       .then(response => res.json(response.data))
       .catch(err => res.status(422).json(err))
   },
-  //Req 2 - Body Locations (General)
+  
+  // Req 2 - Get Body Locations (Specific)
   bodyLocSpecific : (req, res) => {
     //Request Config
-    reqUrl = apiMedicUrl.bodyLoc + req.params.id;
+    reqUrl = apiMedicUrl.bodyLoc + "/" + req.params.id;
+    reqConfig = {
+      params : {"language": "en-gb"},
+      headers : headers
+    }
+    //Axios Request
+    axios
+      .get(reqUrl, reqConfig)
+      .then(response => res.json(response.data))
+      .catch(err => res.status(422).json(err))
+  },
+  
+  // Req 3 - Get Symptoms based on Body Location (Specific)
+  bodySymp : (req, res) => {
+    //Request Config
+    reqUrl = apiMedicUrl.propSymp;
+    reqConfig = {
+      params : {
+        "symptoms": "[234,11]",
+	      "gender": "male",
+	      "year_of_birth": "1984",
+        "language": "en-gb"
+      },
+      headers : headers
+    }
+    //Axios Request
+    axios
+      .get(reqUrl, reqConfig)
+      .then(response => res.json(response.data))
+      .catch(err => res.status(422).json(err))
+  },
+
+  // Req 4 + (5 + 6) Get Symptoms based on Previous Symptom(s)
+  sympSel : (req, res) => {
+    //Request Config
+    reqUrl = apiMedicUrl.bodySymp + "/" + req.params.id + "/" + req.params.gender;
     reqConfig = {
       params : {"language": "en-gb"},
       headers : headers
--- routes/api/apiMedic.js
+++ routes/api/apiMedic.js
@@ -1,17 +1,27 @@
 const router = require("express").Router();
 const apiMedicController = require("../../controllers/apiMedicController");
 
-// Req 1 - Body Location General
-// Matches with "/api/apiMedic/bodyLoc"
+// Matches with "/api/apiMedic
+
+// Req 1 - Get Body Locations General
 router
   .route("/bodyLoc")
   .get(apiMedicController.bodyLocGeneral);
 
-// Req 2 - Body Location Specific
-// Matches with "/api/apiMedic/bodyLoc"
+// Req 2 - Get Body Locations Specific
 router
   .route("/bodyLoc/:id")
   .get(apiMedicController.bodyLocSpecific);
+
+// Req 3 - Get Symptoms of Body Location
+router
+  .route("/bodySymp/:id/:gender")
+  .get(apiMedicController.bodySymp);
+
+// Req 4 + (5 + 6) Get Symptoms based on Previous Symptom(s)
+router
+  .route("/sympSel/:id/:gender")
+  .get(apiMedicController.bodySymp);
 
 
 
