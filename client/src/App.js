c9559ceaa53cc55d472404027ea9f757d49f2de2
Troy Garcia <tg1212@outlook.com>
2019-11-13 17:51:03 -0500

    Redesigned React Routers, Removed Bootstrap Dependendancies, and changed main file structure of front end (Client)

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,23 @@
 // done by arif (+ Troy :3) 
 
-// -------------------- Import Dependencies --------------------------------
+// ---- Import Dependencies ----
 // React Dependencies
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Component Dependencies
-import Nav from "./components/Nav";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 // Page Dependencies 
-import Home from "./pages/home";
-import Diagnosis from "./pages/diagnosis";
-import Trends from "./pages/trends";
-import ErrorPage from "./pages/ErrorPage";
-
+import MainApp from "./pages/MainApp";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />  
-        <Switch>
-          <Route exact path="/" component={Home} /> {/* Root Path - HomePage */}
-          <Route exact path="/diagnosis" component={Diagnosis} /> {/* /Diagnosis Path - Diagnosis Page */}
-          <Route exact path="/trends" component={Trends} />
-          {/* If No Route match - Display Error Page */}
-          <Route component={ErrorPage} />
-        </Switch> 
-        <Footer />
-
-      </div>
-    </Router>
+    <div>
+      <Navbar /> 
+      <MainApp />
+      <Footer />
+    </div>
   );
 }
 
