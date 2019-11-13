99c0834129eed85a48fa5d1578fffe4b5bc14593
neil <arifrahman@hotmail.com>
2019-11-12 19:45:44 -0500

    basic trends page

---
+++ client/src/components/TrendsMap/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+function TrendsMap() {
+  return (
+    <div className="row">
+    <div className="col s6 offset-s2">
+        <p>GoogleMaps Trends page</p>
+    </div>
+    </div>
+  );
+}
+
+export default TrendsMap;
--- client/src/pages/trends.js
+++ client/src/pages/trends.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
 
 import { Link } from "react-router-dom";
+import TrendsForm from "../components/TrendsForm";
+import TrendsMap from "../components/TrendsMap"
+
 
 class Trends extends Component {
     render (){ 
         return (
-            <h3> med 2.0 Trends Page </h3>
+            <body>
+                <h3> med 2.0 Trends Page </h3>
+                <div className="container">
+                    <TrendsForm />
+                    <TrendsMap />
+                </div>
+                
+            </body>
+
+            
             )
     }
 
