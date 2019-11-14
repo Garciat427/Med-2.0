1bbb9c902e60d27409828885a1333c6d91aa3eb1
majedline <majed.atwi@gmail.com>
2019-11-14 02:03:11 -0500

    Added code to build a pieChart based on the API call from the database. Added the chart to the Home.js jsx page. Install 3rd party middle-ware to help: npm install --save react-chartjs-2 chart.js

--- client/src/pages/sub-pages/Home.js
+++ client/src/pages/sub-pages/Home.js
@@ -1,32 +1,72 @@
 import React, { Component } from "react";
+import PieChart from "../../components/PieChart";
+import API from "../../utils/API";
 
 class Home extends Component {
-    render (){ 
+    state = {
+        chartData: {
+            labels: [],
+            data: []
+        }
+    }
+
+    componentDidMount() {
+
+        // Call the API to load the pie chart
+        API.getAllPrimaryDiagnosisInCityInPastWeekPercentage("Hamilton")
+            .then(res => {
+
+                let labelsIn = [];
+                let dataIn = [];
+
+                (res.data).forEach(element => {
+                    labelsIn.push(element.name);
+                    dataIn.push(element.percentage);
+                });
+
+                let newChartData = {
+                    labels: labelsIn,
+                    data: dataIn
+                }
+
+                this.setState({ chartData: newChartData });
+            })
+            .catch(err => console.log(err));
+    }
+
+
+
+    render() {
         return (
             <div className="container">
                 <div className="row">
                     <div className="col s8">
-                        <img src="assets/images/piechart.jpg" alt="pie chart" className= "responsive-img" />                     
+
+                        <PieChart
+                            labels={this.state.chartData.labels}
+                            data={this.state.chartData.data}
+                        />
+
                     </div>
                     <div className="col s4">
                         <h3>Welcome to Med 2.0</h3>
                         <p>
                             We track and follow trends in illnesses in Canada. Check out our Diagnosis Button if you feel you may be ill. Please note we will be using your location, symptoms and diagnosis for our goal of tracking illness. You are welcome to have a look at our trends page as well to see what kind of deseases are currently a concern in your area. Wishing you Good Health!
                         </p>
-                    </div>   
+                    </div>
                 </div>
                 {/* Button Section */}
                 <div className="row">
-                    <div className = "col s8 center">
+                    <div className="col s8 center">
                         <a className="waves-effect waves-light btn-large" href={"/diagnosis"}>Diagnosis</a>
                     </div>
-                    <div className = "col s4 center">
+                    <div className="col s4 center">
                         <a className="waves-effect waves-light btn-large" href={"/trends"}>Trends</a>
                     </div>
                 </div>
             </div>
 
-            )
+        )
     }
 
 }
---
+++ client/src/utils/Helper.js
@@ -0,0 +1,19 @@
+// Helper class that will contain generic helper methods for charts
+class Helper {
+
+    getRandomColor() {
+        let c = '#' + Math.floor(Math.random() * 16777215).toString(16);
+        return c;
+    };
+
+    getRandomColourList(list) {
+        let colorList = [];
+        list.forEach(element => colorList.push(this.getRandomColor()));
+        return colorList;
+    };
+
+
+}
+
+export default Helper
+
