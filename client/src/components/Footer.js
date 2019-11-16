481548c626b846797dbae629c1d14ffeca0644ab
sheenagarcia <tg1212@outlook.com>
2019-11-16 04:55:32 -0500

    Added Page color

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -11,15 +11,17 @@ function Footer() {
     <div className="page-footer deep-purple lighten-1">
       <div className="footer-copyright">
         <div className="container">
-          © 2019 Copyright Med 2.0
-          <p className="white-text right">The MED 2.0 Team (click on the name to visit their GitHub)</p>
-          <ul>
-            <li><a className="grey-text text-lighten-3 right" href="https://github.com/arif2301">- Arifur Rahman </a></li>
-            <li><a className="grey-text text-lighten-3 right" href="https://github.com/lcocard">- Lawrence Cocard </a></li>
-            <li><a className="grey-text text-lighten-3 right" href="https://github.com/majedline">- Majed Atwi </a></li>
-            <li><a className="grey-text text-lighten-3 right " href="https://github.com/Garciat427">Troy Garcia </a></li>
-          </ul>
-          {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
+          <p>
+            © 2019 Copyright Med 2.0
+            <p className="white-text right">The MED 2.0 Team (click on the name to visit their GitHub)</p>
+            <ul>
+              <li><a className="grey-text text-lighten-3 right" href="https://github.com/arif2301">- Arifur Rahman </a></li>
+              <li><a className="grey-text text-lighten-3 right" href="https://github.com/lcocard">- Lawrence Cocard </a></li>
+              <li><a className="grey-text text-lighten-3 right" href="https://github.com/majedline">- Majed Atwi </a></li>
+              <li><a className="grey-text text-lighten-3 right " href="https://github.com/Garciat427">Troy Garcia </a></li>
+            </ul>
+            {/* <a className="grey-text text-lighten-4 right" href="#!">More Links</a> */}
+          </p>
         </div>
       </div>
     </div>
