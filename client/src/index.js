86ddeb2419bcaf67debe2d4744b55e4e0ef96a27
Troy Garcia <50255933+Garciat427@users.noreply.github.com>
2019-11-26 16:30:32 -0500

    Added Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import {createStore} from 'redux';
+
+const store = createStore
 
 ReactDOM.render(<Navbar /> , document.getElementById("rootNavbar"));
 ReactDOM.render(<App />, document.getElementById("root"));
