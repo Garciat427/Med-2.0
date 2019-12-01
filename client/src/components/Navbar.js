7cda4ed603b8319690ce97e46929c2ffcb51a05c
neil <arifrahman@hotmail.com>
2019-12-01 11:57:38 -0500

    pushing changes to download from master

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
         <header>
             <nav>
                 <div class="nav-wrapper deep-purple lighten-1">
-                    <a href="#!" class="brand-logo center" ><i class="large material-icons left">healing</i>MED 2.0</a>
+                    <a href="/." class="brand-logo center" ><i class="large material-icons left">healing</i>MED 2.0</a>
                     <ul class="right">
                         <li><a href="badges.html"><i class="material-icons left">view_module</i>Presentation</a></li>
                         <li><a href="badges.html"><i class="material-icons left">touch_app</i>Login</a></li>
